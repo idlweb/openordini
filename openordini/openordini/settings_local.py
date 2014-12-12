852a008d3d8aed7f293c6fe80ee136951b84e834
idlweb <info@idlweb.net>
2014-12-12 03:17:59 +0100

    modifiche template

--- openordini/openordini/settings_local.py
+++ openordini/openordini/settings_local.py
@@ -3,7 +3,7 @@ from openordini.openordini.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'openordini_antonio',
+        'NAME': 'openordini',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
