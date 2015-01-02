92c7b18177ca289f16338968db33bf07d2a52099
idlweb <info@idlweb.net>
2015-01-02 14:54:29 +0100

    commit admin horizontal_filter

--- openordini/acts_fulfillments/admin.py
+++ openordini/acts_fulfillments/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
+from django import forms
+from django.forms import ModelForm
+from .models import Fascicolo, InstitutionCharge
 
-from .models import Fascicolo
+
+class CustomerInstitutionForm(forms.ModelForm): 
+    def __init__(self, *args, **kwargs):
+        super(CustomerInstitutionForm, self).__init__(*args, **kwargs)
+        wtf = InstitutionCharge.objects.filter();
+        w = self.fields['recipient_set'].widget
+        choices = []
+        for choice in wtf:
+            #choices.append((choice.id, choice.name))
+            choices.append(choice.id)
+        w.choices = choices
 
 class FascicoloAdmin(admin.ModelAdmin):
     list_display = ("status", "approval_date", "publication_date","execution_date","initiative")
-    #list_filter = ["approvazione", ]
-    
+    #list_filter = ["approvazione", ]    
+    #raw_id_fields = ["recipient_set", ]
     search_fields = ["acts_support__support_type", "acts_support__support_date", ]
+    filter_horizontal = ('recipient_set',)
+    #form = CustomerInstitutionForm 
 
 admin.site.register(Fascicolo, FascicoloAdmin)
