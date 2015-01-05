ca5dc4375f5b565d0f57324ef97a01868dd454f7
Francesco Spegni <francesco.spegni@gmail.com>
2015-01-05 18:48:10 +0100

    fix bug in login redirect

--- openordini/acts_fulfillments/views.py
+++ openordini/acts_fulfillments/views.py
@@ -4,6 +4,8 @@ from django.core.exceptions import PermissionDenied
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 from open_municipio.acts.views import ActDetailView
 from .models import Fascicolo
@@ -16,10 +18,26 @@ class FascicoloDetailView(ActDetailView):
         user = self.request.user
         act = super(FascicoloDetailView, self).get_object()
 
-        print "user: %s, act: %s" % (user, act)
+#        print "user: %s, act: %s" % (user, act)
 
-        if user.is_anonymous or user not in act.recipient_set:
-            raise PermissionDenied("foo")
+        allowed_persons = map(lambda c: c.person, act.recipient_set.all())
+
+#        print "allowed: %s" % allowed_persons
+#        print "user: %s (%s)" % (user, user.is_anonymous())
+
+        if user.is_anonymous():
+#            print "it's anonymous..."
+            raise PermissionDenied
+
+#        print "non anonymous..."
+
+        profile = user.get_profile()
+#        print "profile: %s" % profile
+        person = profile.person if profile else None
+#        print "person: %s" % person
+
+        if not person or person not in allowed_persons:
+            raise PermissionDenied
 
         return act
 
@@ -27,5 +45,8 @@ class FascicoloDetailView(ActDetailView):
         try:
             return super(FascicoloDetailView, self).dispatch(request, *args, **kwargs)
         except PermissionDenied, e:
+#            print "error: %s" % e
             messages.warning(request, _("The act you are trying to access is visible only to a subset of the users. You may try to access as a different user, if you have other credentials."))
-            return redirect("auth_login")
+            base_url = reverse("auth_login")
+            full_url = "%s?next=%s" % (base_url, request.path)
+            return HttpResponseRedirect(full_url)
