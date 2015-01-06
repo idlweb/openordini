0a7471f602152669de4f295351542cda6f57b722
idlweb <info@idlweb.net>
2015-01-06 20:14:21 +0100

    commit

--- openordini/openordini/settings.py
+++ openordini/openordini/settings.py
@@ -170,6 +170,7 @@ SUIT_CONFIG = {
         {'app': 'people', 'label': 'Soggetti collegati', 'icon':'icon-user', 'models': ('person','office','institution','group','company','sitting')},       
         {'app': 'acts', 'label': 'Gestione atti del Consiglio', 'icon':'icon-book', 'models': ('act','deliberation','interrogation','interpellation','motion','agenda','amendment','transition','document','attach','speech','calendar')},
         {'app': 'acts_fulfillments', 'label': 'Fascicoli iscritti', 'icon':'icon-book', 'models': ('fascicolo')},
+        {'app': 'oo_payments', 'label': 'Pagamenti', 'icon':'icon-tag', 'models': ('order','payment','subscriptionorder','subscriptionplan')},
         {'app': 'newsletter', 'label': 'Newsletter', 'icon':'icon-comment', 'models': ('newsletter')},
         {'app': 'attendances', 'label': 'Presenze', 'icon':'icon-check', 'models': ('attendance','chargeattendance')},
         {'app': 'events', 'label': 'Eventi', 'icon':'icon-calendar', 'models': ('event')},       
