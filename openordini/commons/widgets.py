bfb5f9a7b87573f84c370ee5c1a8b2468727d872
Francesco Spegni <francesco.spegni@gmail.com>
2015-02-16 10:46:28 +0100

    workon chained selects

--- openordini/commons/widgets.py
+++ openordini/commons/widgets.py
@@ -1,4 +1,4 @@
-from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.contrib.admin.widgets import FilteredSelectMultiple, Select
 
 
 class AdvancedFilteredSelectMultiple(FilteredSelectMultiple):
@@ -17,4 +17,31 @@ class AdvancedFilteredSelectMultiple(FilteredSelectMultiple):
 
         return super(AdvancedFilteredSelectMultiple, self).render(*args, **kwargs)
 
+
+class ChainedSelect(Select):
+    """
+    This select renders a select whose options are linked to a previous one
+    (e.g. through a jquery plugin named chainselect)
+    """
+    
+    def __init__(self, chained_field=None, *args, **kwargs):
+
+        self.chained_field = chained_field
+    
+        super(ChainedSelect, self).__init__(*args, **kwargs)
+
+
+    def render_option(self, selected_choices, option_value, option_label):
+        option_value = force_text(option_value)
+        if option_value in selected_choices:
+            selected_html = mark_safe(' selected="selected"')
+            if not self.allow_multiple_selected:
+                # Only allow for a single selection.
+                selected_choices.remove(option_value)
+        else:
+            selected_html = ''
+        return format_html('<option value="{0}"{1}>{2}</option>',
+                           option_value,
+                           selected_html,
+                           force_text(option_label))
 
