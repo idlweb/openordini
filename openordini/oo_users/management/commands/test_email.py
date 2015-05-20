896d6b3d8bfba30b2682cba7f1a15aea0dbbf86f
idlweb <info@idlweb.net>
2015-05-20 19:36:05 +0200

    test email

---
+++ openordini/oo_users/management/commands/test_email.py
@@ -0,0 +1,48 @@
+from optparse import make_option
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core import mail
+from django.core.management.base import NoArgsCommand, CommandError
+from django.template.loader import render_to_string
+
+import os
+
+class Command(NoArgsCommand):
+
+    help = 'test invio email'
+   
+    def handle_noargs(self, **options):
+
+        self.stdout.write('invio diretto')
+
+        email_list = []
+        template_base_path = os.path.join(settings.PROJECT_ROOT, 'templates/oo_users')
+        email_txt_template_path = os.path.join(template_base_path, 'email.txt')
+        email_html_template_path = os.path.join(template_base_path, 'email.html')
+
+        users_counter = 0
+
+        # build the email for the user
+        email = 'antonio.vangi.av@gmail.com'
+        subject = 'invio email di test'
+        email_sender = 'stafgnpop@psicologipuglia.it' # TODO: replace this address with a meaningful one !
+            
+        msg_text = render_to_string(email_txt_template_path, email_context)
+        msg_html = render_to_string(email_html_template_path, email_context)
+
+        msg = mail.EmailMultiAlternatives(subject, msg_text, email_sender, [email])
+
+        msg.attach_alternative(msg_html, 'text/html')
+
+        # add the email to the list of emails that will be sent
+        email_list.append(msg)
+
+
+        if email_list:
+            self.stdout.write('Start sending emails to all the users ...')
+                # use default email connection
+                connection = mail.get_connection()
+
+            # send all the emails in a single call, using a single connection
+            connection.send_messages(email_list)
