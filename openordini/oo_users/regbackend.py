01a81c4245d8133bb3e39a12097ed9055e1fa307
idlweb <opencorato>
2015-07-17 10:16:50 +0200

    Fixes #35

--- openordini/oo_users/regbackend.py
+++ openordini/oo_users/regbackend.py
@@ -175,12 +175,12 @@ def finalize_registration(self, user):
     	print user.says_is_self_employed
         is_registered_a = (user.register_subscription_date != None) and (user.says_is_psicologo_lavoro or user.says_is_psicologo_clinico or user.says_is_psicologo_forense or user.says_is_self_employed)
         print is_registered_a
-        if is_registered_a:
-        	print "test 1 - inserisco in albo -> is_registered_a"
-        	i = Institution.objects.get(slug="sezione-a")             	
-        	#if InstitutionCharge.objects.filter(person__pk=user.person.id).filter(institution=i).count()  == 0:        		
-        	member_charge = InstitutionCharge(person=user.person, institution=i, start_date=user.register_subscription_date) 
-        	member_charge.save() 
+        #if is_registered_a:
+        print "test 1 - inserisco in albo -> is_registered_a"
+        i = Institution.objects.get(slug="sezione-a")             	
+        #if InstitutionCharge.objects.filter(person__pk=user.person.id).filter(institution=i).count()  == 0:        		
+        member_charge = InstitutionCharge(person=user.person, institution=i, start_date=user.register_subscription_date) 
+        member_charge.save() 
 
         if user.says_is_psicologo_lavoro:
             g, created = Group.objects.get_or_create(name=settings.SYSTEM_GROUP_NAMES["psicologo_lavoro"])
