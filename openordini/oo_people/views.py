a2d34144d60bc7f396f5395d6f8aac7f94fdab29
idlweb <opencorato>
2015-08-11 10:10:40 +0200

    fix people search

--- openordini/oo_people/views.py
+++ openordini/oo_people/views.py
@@ -219,10 +219,10 @@ class OOPoliticianSearchView(PoliticianSearchView):
                 }
             }      
 
-            if person.userprofile and person.userprofile.userprofile #and \
-                    person.userprofile.userprofile.anagrafica:
-                p_data["fields"]["extra_data"] = person.userprofile.userprofile.anagrafica.studio
-
+            if person.userprofile and person.userprofile.userprofile: #and \
+                    #person.userprofile.userprofile.anagrafica:
+                #p_data["fields"]["extra_data"] = person.userprofile.userprofile.anagrafica.studio
+                pass
 
             results.append(p_data)
 
