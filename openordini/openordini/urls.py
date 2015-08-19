7559325a7f82dd7ebb4dfce054c1c4733b81099c
Antonio <info@idlweb.net>
2015-08-19 13:10:24 +0200

    Update urls.py

--- openordini/openordini/urls.py
+++ openordini/openordini/urls.py
@@ -33,5 +33,5 @@ urlpatterns = patterns('',
     url('^institutions/', include('openordini.oo_people.urls.institutions')),
     url('^users/', include('openordini.oo_users.urls')),
     url('^payments/', include('openordini.oo_payments.urls')),
-)
+) + urlpatterns
 
