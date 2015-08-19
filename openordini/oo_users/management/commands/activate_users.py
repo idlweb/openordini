513e14f2be38bd44e5e6f264448e286ef27fa5c3
Antonio <info@idlweb.net>
2015-08-20 01:29:19 +0200

    Update activate_users.py

--- openordini/oo_users/management/commands/activate_users.py
+++ openordini/oo_users/management/commands/activate_users.py
@@ -47,7 +47,7 @@ class Command(NoArgsCommand):
 
         users_counter = 0
 
-        for u in User.objects.all().order_by("last_name")[0:5]:
+        for u in User.objects.all().order_by("last_name")[0:2]:
             print "-------------------------------- test utenti"
             #print vars(u)
             #if not u.is_active:
