f1e767d5a844894d9ceccc5481cb03f91c5aaaa2
Antonio <info@idlweb.net>
2015-08-25 02:28:12 +0200

    Update models.py

--- openordini/oo_email/models.py
+++ openordini/oo_email/models.py
@@ -9,3 +9,11 @@ class recordo_login_by_email(models.Model):
     password_email = models.CharField(max_length=255)
     username_email = models.CharField(max_length=255)
     ref_email = models.ForeignKey(EmailMessage)
+    
+def __unicode__(self):
+        return self.username_email
+
+
+    class Meta:
+        verbose_name = _("credenziali da email")
+        verbose_name_plural = _("credenziali da email")
