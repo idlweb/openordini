50a1958385b52964edc6fd26988a7a2b03c8cb59
idlweb <info@idlweb.net>
2015-08-25 02:54:44 +0200

    we have added the admin panel

---
+++ openordini/oo_email/admin.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns
+from django.contrib import admin
+from openordini.oo_email.models import recordo_login_by_email
+
+class RecordoLoginByEmailAdmin(admin.ModelAdmin):
+    list_display = ('username_email',)
+    #list_filter = (VerificheListFilter,)
+    search_fields = ["username_email", ]
+    #actions = ['validazione_casellario','export_come_JSON','export_selected_objects']
+
+    
+admin.site.register(recordo_login_by_email, RecordoLoginByEmailAdmin)
