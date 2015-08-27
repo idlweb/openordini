85942a88539dbdd050edd16e2915eaa8ca2090a7
Antonio <info@idlweb.net>
2015-08-28 01:27:35 +0200

    Update admin.py

--- openordini/oo_email/admin.py
+++ openordini/oo_email/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from openordini.oo_email.models import recordo_login_by_email
 
 class RecordoLoginByEmailAdmin(admin.ModelAdmin):
-    list_display = ('username_email',)
+    list_display = ('username_email', 'utente_email__email')
     #list_filter = (VerificheListFilter,)
     search_fields = ["username_email", "utente_email__email" ]
     #actions = ['validazione_casellario','export_come_JSON','export_selected_objects']
