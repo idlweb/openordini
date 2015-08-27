9974e83ae1663a065140195ec4b59e95d2622b39
Antonio <info@idlweb.net>
2015-08-28 01:18:44 +0200

    Update admin.py

--- openordini/oo_email/admin.py
+++ openordini/oo_email/admin.py
@@ -5,7 +5,7 @@ from openordini.oo_email.models import recordo_login_by_email
 class RecordoLoginByEmailAdmin(admin.ModelAdmin):
     list_display = ('username_email',)
     #list_filter = (VerificheListFilter,)
-    search_fields = ["username_email", ]
+    search_fields = ["username_email", "utente_email__related_fieldname" ]
     #actions = ['validazione_casellario','export_come_JSON','export_selected_objects']
 
     
