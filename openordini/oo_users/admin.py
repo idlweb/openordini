b20d6f4dc5c8090757e86207b58729641158ab43
Antonio <info@idlweb.net>
2015-08-30 16:53:10 +0200

    Update admin.py

--- openordini/oo_users/admin.py
+++ openordini/oo_users/admin.py
@@ -20,7 +20,7 @@ from .regbackend import finalize_registration
 from .forms import CustomAjaxModelFormView 
 from django.contrib.auth.models import User, Group
 
-from openordini.commons.subscription_email import *
+from openordini.commons.subscription_email import picked_email_to_send
 
 
 #from django.utils.html import escape
@@ -87,7 +87,8 @@ class UserProfileAdmin(CustomAjaxModelAdmin):
     actions = ['send_email_on_selection',]
     
     def send_email_on_selection(self, request, queryset):
-        send_email_picked(queryset)
+        sap = send_email_picked()
+        sap.send_email_picked(queryset)
         rows_updated = queryset.update(email_login_inviata=True)
         if rows_updated == 1:
             message_bit = "email inviata"
