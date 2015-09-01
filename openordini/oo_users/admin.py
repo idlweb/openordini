1ef61b4bf3403401411c26e2c31c2b68760072fb
Antonio <info@idlweb.net>
2015-09-02 00:42:22 +0200

    Update admin.py

--- openordini/oo_users/admin.py
+++ openordini/oo_users/admin.py
@@ -88,9 +88,14 @@ class UserProfileAdmin(CustomAjaxModelAdmin):
     
     def send_email_on_selection(self, request, queryset):
         ioc = subscription_email.picked_email_to_send()
-        ioc.send_email_picked(queryset)
-
-        rows_updated = queryset.update(email_login_inviata=True)
+        
+        t_email = ioc.send_email_picked(queryset)
+        
+        if t_email = True:
+            rows_updated = queryset.update(email_login_inviata=True)
+        else:
+            rows_updated = queryset.update(email_login_inviata=False)
+            
         if rows_updated == 1:
             message_bit = "email inviata"
         else:
